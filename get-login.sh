02ed95bf660409f253183421664f283ca7da48fb
Chris Solomon <Chris.Solomon@eroad.com>
2020-09-17 17:18:15 +1200

    Updated the get-login.sh to be compatable with AWS CLI 2.x - AWS CLI 2.x removed the depecated get-login option, so updated to use the get-login-password option.

--- get-login.sh
+++ get-login.sh
@@ -5,4 +5,5 @@ export OKTA_PASSWORD=${OKTA_PASSWORD:=}
 export ROLE_NUMBER_SELECTION=${ROLE_NUMBER_SELECTION:=-1}
 okta-awscli -u ${OKTA_USERNAME} -p ${OKTA_PASSWORD} -r ${ROLE_NUMBER_SELECTION}
 
-$(aws ecr get-login --no-include-email --region ap-southeast-2)
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 212136148154.dkr.ecr.ap-southeast-2.amazonaws.com
+
